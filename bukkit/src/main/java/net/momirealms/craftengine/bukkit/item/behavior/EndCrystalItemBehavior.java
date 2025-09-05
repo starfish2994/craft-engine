1da10934451a2f08105e68127727cfeac8e59676
halogly <Halogly2004@gmail.com>
2025-09-05 15:44:48 +0800

    稍微减少冗余代码

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/item/behavior/EndCrystalItemBehavior.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/item/behavior/EndCrystalItemBehavior.java
@@ -1,5 +1,6 @@
 package net.momirealms.craftengine.bukkit.item.behavior;
 
+import net.momirealms.craftengine.bukkit.entity.BukkitEntity;
 import net.momirealms.craftengine.bukkit.nms.FastNMS;
 import net.momirealms.craftengine.bukkit.plugin.reflection.minecraft.MBlocks;
 import net.momirealms.craftengine.bukkit.util.BlockStateUtils;
@@ -14,9 +15,13 @@ import net.momirealms.craftengine.core.util.Key;
 import net.momirealms.craftengine.core.world.BlockPos;
 import net.momirealms.craftengine.core.world.World;
 import net.momirealms.craftengine.core.world.collision.AABB;
+import org.bukkit.GameMode;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Player;
+import org.bukkit.util.BoundingBox;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -43,26 +48,35 @@ public class EndCrystalItemBehavior extends ItemBehavior {
             if (aboveBlockOwner != MBlocks.AIR) {
                 return InteractionResult.PASS;
             } else {
-
                 double x = abovePos.x();
                 double y = abovePos.y();
                 double z = abovePos.z();
-                AABB aabb = new AABB(x, y, z, x + 1.0, y + 2.0, z + 1.0);
-                List<Entity> entities = world.getEntities(null, aabb);
-                if (!entities.isEmpty()) {
-                    return InteractionResult.PASS;
-                }
+                Object level = world.serverWorld();
+                org.bukkit.World bukkitWorld = (org.bukkit.World) world.platformWorld();
 
-                List<AABB> aabbs = List.of(aabb);
-                List<Object> nmsAABB = aabbs.stream()
-                        .map(AABB -> FastNMS.INSTANCE.constructor$AABB(
-                                AABB.minX, AABB.minY, AABB.minZ,
-                                AABB.maxX, AABB.maxY, AABB.maxZ
-                        )).toList();
-                if (!FastNMS.INSTANCE.checkEntityCollision(context.getLevel().serverWorld(), nmsAABB, x, y, z)) {
-                    return InteractionResult.PASS;
+                AABB aabb = new AABB(x, y, z, x + 1.0, y + 2.0, z + 1.0);
+                BoundingBox bukkitAABB = new BoundingBox(aabb.minX, aabb.minY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.maxZ);
+                // 忽略旁观者
+                List<Entity> entities = new ArrayList<>();
+                for (org.bukkit.entity.Entity bukkitEntity : bukkitWorld.getNearbyEntities(bukkitAABB)) {
+                    if (bukkitEntity instanceof Player player && player.getGameMode().equals(GameMode.SPECTATOR)) {
+                        continue;
+                    }
+                    Entity entity = new BukkitEntity(bukkitEntity);
+                    entities.add(entity);
                 }
-                return InteractionResult.SUCCESS;
+                // 检测家具实体
+                List<AABB> detectionAABBs = List.of(aabb);
+                List<Object> nmsAABBs = detectionAABBs.stream()
+                        .map(aabbs -> FastNMS.INSTANCE.constructor$AABB(aabbs.minX, aabbs.minY, aabbs.minZ, aabbs.maxX, aabbs.maxY, aabbs.maxZ))
+                        .toList();
+                boolean hasEntities = FastNMS.INSTANCE.checkEntityCollision(level, nmsAABBs, x + 0.5, y, z + 0.5);
+
+                if (!entities.isEmpty() || !hasEntities) {
+                    return InteractionResult.PASS;
+                } else {
+                    return InteractionResult.SUCCESS;
+                }
             }
         }
     }
