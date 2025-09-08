3fb7efee6edbcba24cca9fddfe3a6bccda801d89
jhqwqmc <2110242767@qq.com>
2025-09-08 20:09:09 +0800

    feat(bukkit): 添加沙发

--- core/src/main/java/net/momirealms/craftengine/core/pack/AbstractPackManager.java
+++ core/src/main/java/net/momirealms/craftengine/core/pack/AbstractPackManager.java
@@ -465,7 +465,7 @@ public abstract class AbstractPackManager implements PackManager {
         plugin.saveResource("resources/default/resourcepack/assets/minecraft/models/block/custom/pebble_3.json");
         plugin.saveResource("resources/default/resourcepack/assets/minecraft/models/item/custom/sofa_straight.json");
         plugin.saveResource("resources/default/resourcepack/assets/minecraft/models/item/custom/sofa_inner.json");
-        plugin.saveResource("resources/default/resourcepack/assets/minecraft/models/block/custom/sofa.json");
+        plugin.saveResource("resources/default/resourcepack/assets/minecraft/models/item/custom/sofa.json");
         plugin.saveResource("resources/default/resourcepack/assets/minecraft/textures/item/custom/sofa.png");
 
         // ores
