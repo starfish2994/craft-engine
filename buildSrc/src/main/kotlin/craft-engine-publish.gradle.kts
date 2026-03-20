a8836bd203379c7ee6b13c09d05e65308280ffe6
Catnies <catnies@foxmail.com>
2026-03-21 00:15:26 +0800

    chore: 修改 Publish 脚本.

--- buildSrc/src/main/kotlin/craft-engine-publish.gradle.kts
+++ buildSrc/src/main/kotlin/craft-engine-publish.gradle.kts
@@ -1,5 +1,4 @@
 import java.net.URI
-import java.util.Properties
 
 plugins {
     `maven-publish`
@@ -8,22 +7,15 @@ plugins {
 val projectVersion = project.rootProject.property("project_version").toString()
 val isSnapshot = projectVersion.endsWith("-SNAPSHOT")
 
-val credentialsFile = file("publishing-credentials.properties")
-val credentialsProps = Properties().apply {
-    if (credentialsFile.exists()) {
-        load(credentialsFile.inputStream())
-    }
-}
-
 publishing {
     repositories {
         maven {
             val repoName = if (isSnapshot) "snapshots" else "releases"
-            name = repoName
+            name = "XiaoMoMi"
             url = URI("https://repo.momirealms.net/$repoName")
-            credentials {
-                username = credentialsProps.getProperty("repoUsername") ?: System.getenv("REPO_USERNAME")
-                password = credentialsProps.getProperty("repoPassword") ?: System.getenv("REPO_PASSWORD")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
             }
         }
     }
