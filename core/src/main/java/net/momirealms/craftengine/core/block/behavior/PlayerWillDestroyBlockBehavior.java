ad885fe87f71a2cad72ad8df8fd52e9aa1e3d7e6
XiaoMoMi <littlemomi@qq.com>
2026-04-09 08:10:25 +0800

    补充注释

--- core/src/main/java/net/momirealms/craftengine/core/block/behavior/PlayerWillDestroyBlockBehavior.java
+++ core/src/main/java/net/momirealms/craftengine/core/block/behavior/PlayerWillDestroyBlockBehavior.java
@@ -2,5 +2,13 @@ package net.momirealms.craftengine.core.block.behavior;
 
 public interface PlayerWillDestroyBlockBehavior {
 
+    /**
+     * <code>args[0]</code>: Level (level)<br>
+     * <code>args[1]</code>: BlockPos (pos)<br>
+     * <code>args[2]</code>: BlockState (state)<br>
+     * <code>args[3]</code>: Player (player)<br>
+     * <p>
+     * Returns: BlockState
+     */
     Object playerWillDestroy(Object thisBlock, Object[] args);
 }
