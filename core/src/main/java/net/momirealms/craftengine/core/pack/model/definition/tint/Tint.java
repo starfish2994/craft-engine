dbed4b450b6208737e1f502b35337a502ee20474
XiaoMoMi <littlemomi@qq.com>
2026-04-12 19:36:21 +0800

    避免未配置默认值导致错误

--- core/src/main/java/net/momirealms/craftengine/core/pack/model/definition/tint/Tint.java
+++ core/src/main/java/net/momirealms/craftengine/core/pack/model/definition/tint/Tint.java
@@ -10,15 +10,18 @@ import java.util.function.Supplier;
 public interface Tint extends Supplier<JsonObject> {
 
     default void applyTint(JsonObject json, Either<Integer, List<Float>> value, String key) {
-        if (value == null) return;
-        value.ifLeft(i -> {
-            json.addProperty(key, i);
-        }).ifRight(list -> {
-            JsonArray array = new JsonArray();
-            for (float i : list) {
-                array.add(i);
-            }
-            json.add(key, array);
-        });
+        if (value == null) {
+            json.addProperty(key, 16777215);
+        } else {
+            value.ifLeft(i -> {
+                json.addProperty(key, i);
+            }).ifRight(list -> {
+                JsonArray array = new JsonArray();
+                for (float i : list) {
+                    array.add(i);
+                }
+                json.add(key, array);
+            });
+        }
     }
 }
