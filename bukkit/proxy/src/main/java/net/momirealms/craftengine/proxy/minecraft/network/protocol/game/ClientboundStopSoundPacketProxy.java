1c9432f1b634981d559199ee74a9225dd904888b
XiaoMoMi <littlemomi@qq.com>
2026-05-04 00:29:22 +0800

    Update ClientboundStopSoundPacketProxy.java

--- bukkit/proxy/src/main/java/net/momirealms/craftengine/proxy/minecraft/network/protocol/game/ClientboundStopSoundPacketProxy.java
+++ bukkit/proxy/src/main/java/net/momirealms/craftengine/proxy/minecraft/network/protocol/game/ClientboundStopSoundPacketProxy.java
@@ -3,6 +3,7 @@ package net.momirealms.craftengine.proxy.minecraft.network.protocol.game;
 import net.momirealms.craftengine.proxy.minecraft.resources.IdentifierProxy;
 import net.momirealms.craftengine.proxy.minecraft.sounds.SoundSourceProxy;
 import net.momirealms.sparrow.reflection.proxy.ASMProxyFactory;
+import net.momirealms.sparrow.reflection.proxy.annotation.ConstructorInvoker;
 import net.momirealms.sparrow.reflection.proxy.annotation.ReflectionProxy;
 import net.momirealms.sparrow.reflection.proxy.annotation.Type;
 
@@ -10,6 +11,7 @@ import net.momirealms.sparrow.reflection.proxy.annotation.Type;
 public interface ClientboundStopSoundPacketProxy {
     ClientboundStopSoundPacketProxy INSTANCE = ASMProxyFactory.create(ClientboundStopSoundPacketProxy.class);
 
+    @ConstructorInvoker
     Object newInstance(@Type(clazz = IdentifierProxy.class) Object soundId,
                        @Type(clazz = SoundSourceProxy.class) Object soundSource);
 }
