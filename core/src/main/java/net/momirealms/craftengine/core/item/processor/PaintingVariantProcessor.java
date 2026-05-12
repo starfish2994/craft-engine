afe04e46847056aaed75de2a306d97c0b83af936
jhqwqmc <jhcraft@qq.com>
2026-05-12 08:56:48 +0800

    实现一个数据类型

---
+++ core/src/main/java/net/momirealms/craftengine/core/item/processor/PaintingVariantProcessor.java
@@ -0,0 +1,45 @@
+package net.momirealms.craftengine.core.item.processor;
+
+import net.momirealms.craftengine.core.item.Item;
+import net.momirealms.craftengine.core.item.ItemBuildContext;
+import net.momirealms.craftengine.core.item.component.DataComponentKeys;
+import net.momirealms.craftengine.core.plugin.config.ConfigValue;
+import net.momirealms.craftengine.core.util.Key;
+import net.momirealms.craftengine.core.util.VersionHelper;
+
+import java.util.Map;
+
+public final class PaintingVariantProcessor implements SimpleNetworkItemProcessor {
+    public static final ItemProcessorFactory<PaintingVariantProcessor> FACTORY = new Factory();
+    public final String id;
+
+    public PaintingVariantProcessor(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public Item apply(Item item, ItemBuildContext context) {
+        if (VersionHelper.isOrAbove1_21_5()) {
+            item.setJavaComponent(DataComponentKeys.PAINTING_VARIANT, this.id);
+        } else {
+            item.setJavaComponent(DataComponentKeys.ENTITY_DATA, Map.of(
+                    "id", "minecraft:painting",
+                    "variant", this.id
+            ));
+        }
+        return item;
+    }
+
+    @Override
+    public Key componentType(Item item, ItemBuildContext context) {
+        return VersionHelper.isOrAbove1_21_5() ? DataComponentKeys.PAINTING_VARIANT : DataComponentKeys.ENTITY_DATA;
+    }
+
+    private static class Factory implements ItemProcessorFactory<PaintingVariantProcessor> {
+
+        @Override
+        public PaintingVariantProcessor create(ConfigValue value) {
+            return new PaintingVariantProcessor(value.getAsIdentifier().asString());
+        }
+    }
+}
