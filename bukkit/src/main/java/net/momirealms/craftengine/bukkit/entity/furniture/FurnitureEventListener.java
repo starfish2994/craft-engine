0aeea668d8be76dcfc94120a5784465ad4f8ab08
XiaoMoMi <littlemomi@qq.com>
2026-05-14 01:26:59 +0800

    Update FurnitureEventListener.java

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/entity/furniture/FurnitureEventListener.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/entity/furniture/FurnitureEventListener.java
@@ -48,9 +48,6 @@ public final class FurnitureEventListener implements Listener {
      */
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void onEntitiesLoadEarly(EntitiesLoadEvent event) {
-        if (!event.getChunk().isLoaded()) {
-            return;
-        }
         List<Entity> entities = event.getEntities();
         for (int i = 0, size = entities.size(); i < size; i++) {
             Entity entity = entities.get(i);
