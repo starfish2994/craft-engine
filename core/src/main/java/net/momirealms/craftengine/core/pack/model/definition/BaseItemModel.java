5c92f8af6cff16cd1045b14507b3002f6eba065a
jhqwqmc <jhcraft@qq.com>
2026-05-29 00:09:39 +0800

    修复transformation多版本生成

--- core/src/main/java/net/momirealms/craftengine/core/pack/model/definition/BaseItemModel.java
+++ core/src/main/java/net/momirealms/craftengine/core/pack/model/definition/BaseItemModel.java
@@ -8,6 +8,7 @@ import net.momirealms.craftengine.core.pack.model.definition.tint.Tints;
 import net.momirealms.craftengine.core.pack.model.generation.ModelGeneration;
 import net.momirealms.craftengine.core.pack.model.generation.ModelGenerationHolder;
 import net.momirealms.craftengine.core.pack.revision.Revision;
+import net.momirealms.craftengine.core.pack.revision.Revisions;
 import net.momirealms.craftengine.core.plugin.config.ConfigSection;
 import net.momirealms.craftengine.core.util.Key;
 import net.momirealms.craftengine.core.util.MinecraftVersion;
@@ -99,6 +100,9 @@ public final class BaseItemModel implements ItemModel {
 
     @Override
     public void gatherRevisions(Consumer<Revision> consumer) {
+        if (this.transformation != null) {
+            consumer.accept(Revisions.SINCE_26_1);
+        }
     }
 
     private static class Factory implements ItemModelFactory<BaseItemModel> {
