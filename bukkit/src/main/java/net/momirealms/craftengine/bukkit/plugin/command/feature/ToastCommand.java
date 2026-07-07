16c0ba0b3abe175f35147b4110c70ef0a8f27904
XiaoMoMi <70987828+Xiao-MoMi@users.noreply.github.com>
2026-07-07 10:45:24 +0800

    添加玩家选择器

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/command/feature/ToastCommand.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/command/feature/ToastCommand.java
@@ -7,6 +7,7 @@ import net.momirealms.craftengine.bukkit.plugin.command.BukkitCommandFeature;
 import net.momirealms.craftengine.bukkit.plugin.user.BukkitServerPlayer;
 import net.momirealms.craftengine.bukkit.util.KeyUtils;
 import net.momirealms.craftengine.core.advancement.AdvancementType;
+import net.momirealms.craftengine.core.item.BuildableItem;
 import net.momirealms.craftengine.core.item.Item;
 import net.momirealms.craftengine.core.plugin.CraftEngine;
 import net.momirealms.craftengine.core.plugin.command.CraftEngineCommandManager;
@@ -21,7 +22,9 @@ import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.incendo.cloud.Command;
 import org.incendo.cloud.CommandManager;
+import org.incendo.cloud.bukkit.data.MultiplePlayerSelector;
 import org.incendo.cloud.bukkit.parser.NamespacedKeyParser;
+import org.incendo.cloud.bukkit.parser.selector.MultiplePlayerSelectorParser;
 import org.incendo.cloud.context.CommandContext;
 import org.incendo.cloud.context.CommandInput;
 import org.incendo.cloud.parser.standard.EnumParser;
@@ -31,6 +34,7 @@ import org.incendo.cloud.suggestion.SuggestionProvider;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 public final class ToastCommand extends BukkitCommandFeature<CommandSender> {
@@ -42,8 +46,8 @@ public final class ToastCommand extends BukkitCommandFeature<CommandSender> {
     @Override
     public Command.Builder<? extends CommandSender> assembleCommand(CommandManager<CommandSender> manager, Command.Builder<CommandSender> builder) {
         return builder
-                .senderType(Player.class)
                 .flag(FlagKeys.SILENT_FLAG)
+                .required("players", MultiplePlayerSelectorParser.multiplePlayerSelectorParser(false))
                 .required("type", EnumParser.enumComponent(AdvancementType.class))
                 .required("item", NamespacedKeyParser.namespacedKeyComponent().suggestionProvider(new SuggestionProvider<>() {
                     @Override
@@ -57,20 +61,29 @@ public final class ToastCommand extends BukkitCommandFeature<CommandSender> {
                 }))
                 .required("message", StringParser.stringComponent(StringParser.StringMode.GREEDY_FLAG_YIELDING))
                 .handler(context -> {
+                    MultiplePlayerSelector selector = context.get("players");
+                    Collection<Player> players = selector.values();
                     AdvancementType type = context.get("type");
                     NamespacedKey namespacedKey = context.get("item");
                     Key key = KeyUtils.namespacedKeyToKey(namespacedKey);
                     String message = context.get("message");
-                    BukkitServerPlayer serverPlayer = BukkitAdaptor.adapt(context.sender());
-                    if (serverPlayer == null) return;
-                    Item icon = Item.byId(key, serverPlayer);
-                    if (icon == null) {
+                    Optional<? extends BuildableItem> buildableItem = plugin().itemManager().getBuildableItem(key);
+                    if (buildableItem.isEmpty()) {
                         handleFeedback(context, MessageConstants.COMMAND_TOAST_FAILURE_ITEM_NOT_EXIST, Component.text(key.toString()));
                         return;
                     }
-                    Component text = AdventureHelper.miniMessage().deserialize(AdventureHelper.legacyToMiniMessage(message), PlayerOptionalContext.of(serverPlayer).tagResolvers());
-                    serverPlayer.sendToast(text, icon, type);
-                    handleFeedback(context, MessageConstants.COMMAND_TOAST_SUCCESS, Component.text(key.toString()), Component.text(serverPlayer.name()));
+                    for (Player player : players) {
+                        BukkitServerPlayer serverPlayer = BukkitAdaptor.adapt(player);
+                        if (serverPlayer == null) continue;
+                        Item icon = buildableItem.get().buildItem(serverPlayer);
+                        Component text = AdventureHelper.miniMessage().deserialize(AdventureHelper.legacyToMiniMessage(message), PlayerOptionalContext.of(serverPlayer).tagResolvers());
+                        serverPlayer.sendToast(text, icon, type);
+                    }
+                    if (players.size() == 1) {
+                        handleFeedback(context, MessageConstants.COMMAND_TOAST_SUCCESS_SINGLE, Component.text(key.toString()), Component.text(players.iterator().next().getName()));
+                    } else if (players.size() > 1) {
+                        handleFeedback(context, MessageConstants.COMMAND_TOAST_SUCCESS_MULTIPLE, Component.text(key.toString()), Component.text(players.size()));
+                    }
                 });
     }
 
