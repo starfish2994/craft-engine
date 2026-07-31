1364d2cb713ec7720589fa12697bcff18937bb77
XiaoMoMi <70987828+Xiao-MoMi@users.noreply.github.com>
2026-07-31 18:02:38 +0800

    移除版本条件

--- bukkit/proxy/src/main/java/net/momirealms/craftengine/proxy/minecraft/network/chat/contents/PlainTextContentsProxy.java
+++ bukkit/proxy/src/main/java/net/momirealms/craftengine/proxy/minecraft/network/chat/contents/PlainTextContentsProxy.java
@@ -5,7 +5,7 @@ import net.momirealms.sparrow.reflection.proxy.ASMProxyFactory;
 import net.momirealms.sparrow.reflection.proxy.annotation.MethodInvoker;
 import net.momirealms.sparrow.reflection.proxy.annotation.ReflectionProxy;
 
-@ReflectionProxy(name = {"net.minecraft.network.chat.contents.PlainTextContents", "net.minecraft.network.chat.contents.LiteralContents"}, activeIf = "min_version=1.20.3")
+@ReflectionProxy(name = {"net.minecraft.network.chat.contents.PlainTextContents", "net.minecraft.network.chat.contents.LiteralContents"})
 public interface PlainTextContentsProxy {
     PlainTextContentsProxy INSTANCE = ASMProxyFactory.create(PlainTextContentsProxy.class);
     Class<?> CLASS = SparrowClass.find("net.minecraft.network.chat.contents.PlainTextContents");
