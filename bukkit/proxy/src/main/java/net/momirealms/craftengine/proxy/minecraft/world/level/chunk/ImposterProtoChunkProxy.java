c915aed5fe93aa46b81e0548269229b510d81988
XiaoMoMi <70987828+Xiao-MoMi@users.noreply.github.com>
2026-08-09 03:08:28 +0800

    服务端已自带去重，简化逻辑

--- bukkit/proxy/src/main/java/net/momirealms/craftengine/proxy/minecraft/world/level/chunk/ImposterProtoChunkProxy.java
+++ bukkit/proxy/src/main/java/net/momirealms/craftengine/proxy/minecraft/world/level/chunk/ImposterProtoChunkProxy.java
@@ -5,7 +5,7 @@ import net.momirealms.sparrow.reflection.proxy.ASMProxyFactory;
 import net.momirealms.sparrow.reflection.proxy.annotation.FieldGetter;
 import net.momirealms.sparrow.reflection.proxy.annotation.ReflectionProxy;
 
-@ReflectionProxy(name = "net.minecraft.world.level.chunk.ImposterProtoChunk")
+@ReflectionProxy(name = "net.minecraft.world.level.chunk.ImposterProtoChunk", activeIf = "min_version=1.21.4")
 public interface ImposterProtoChunkProxy {
     ImposterProtoChunkProxy INSTANCE = ASMProxyFactory.create(ImposterProtoChunkProxy.class);
     Class<?> CLASS = SparrowClass.find("net.minecraft.world.level.chunk.ImposterProtoChunk");
