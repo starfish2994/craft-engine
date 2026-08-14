608ac8c5dbcf9a7ce7acd346c5d5a9911f673ff3
XiaoMoMi <70987828+Xiao-MoMi@users.noreply.github.com>
2026-08-15 04:42:08 +0800

    任意实体属性

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/attribute/PaperAttributeEventListener.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/attribute/PaperAttributeEventListener.java
@@ -1,11 +1,16 @@
 package net.momirealms.craftengine.bukkit.attribute;
 
+import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent;
+import com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent;
 import io.papermc.paper.event.entity.EntityEquipmentChangedEvent;
+import net.momirealms.craftengine.bukkit.api.BukkitAdaptor;
+import net.momirealms.craftengine.bukkit.entity.BukkitEntity;
 import net.momirealms.craftengine.bukkit.util.EquipmentSlotUtils;
 import net.momirealms.craftengine.bukkit.util.ItemStackUtils;
 import net.momirealms.craftengine.core.attribute.AttributeContainer;
 import net.momirealms.craftengine.core.attribute.equipment.EntityEquipments;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.EquipmentSlot;
@@ -36,6 +41,22 @@ public final class PaperAttributeEventListener implements Listener {
             EquipmentSlot slot = entry.getKey();
             EntityEquipmentChangedEvent.EquipmentChange changed = entry.getValue();
             equipments.add(EquipmentSlotUtils.toEquipmentSetSlot(slot), ItemStackUtils.wrap(changed.newItem()));
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityAdd(EntityAddToWorldEvent event) {
+        if (event.getEntity() instanceof LivingEntity livingEntity) {
+            if (livingEntity instanceof Player) return;
+            this.manager.getOrCreateContainer(BukkitAdaptor.adapt(livingEntity));
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityRemove(EntityRemoveFromWorldEvent event) {
+        if (event.getEntity() instanceof LivingEntity livingEntity) {
+            if (livingEntity instanceof Player) return;
+            this.manager.removeContainer(livingEntity.getUniqueId());
         }
     }
 }
